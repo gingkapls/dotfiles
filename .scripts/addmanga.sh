8f9adf8dfb52bb1b6560877294154908363f0382
Divyang <divyang_2k10@yahoo.com>
2020-11-26 15:10:19 +0530

    added chapter descriptions

--- .scripts/addmanga.sh
+++ .scripts/addmanga.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-pushd "$1"
+pushd "$1" 
 series="$(pwd | awk -F "/" '{print $NF}')"
 
 while read -r manga; do 
-	name="$(echo $manga | perl -pe 's/Vol\..\d\s//;' -pe 's/Ch.\d*\K.+//g;' -pe 's/\d+/sprintf("%02d",$&)/ge;' -pe 's/(.*)/\U$1/g')" ## Cleaning up the chapter name
+	name="$(echo $manga | perl -pe 's/Vol\..\d\s//;' -e 's/Ch.\d*\K.+//g;' -e 's/\d+/sprintf("%02d",$&)/ge;' -e 's/(.*)/\U$1/g')" ## Cleaning up the chapter name
+	chapter="$(echo $manga | perl -pe 's/.*-//g;' -e 's/\(gb\).*|^\s*//g;' -e 's/\s$//g' )"
 #	echo -ne "\r $PWD/$manga"
 	echo -ne "\r $series $name"
+	echo " $chapter"
 	 while read page; do 
 	 	aspect="$(file "$page" | perl -pe "s/\d.*\Kx.\K\d*\K.*//g;" -pe "s/.*,\s/scale=4; /g;" -pe "s/x/ \/ /" | bc)"
 	 	if [ "$(echo "$aspect <= 1" | bc)" -eq 1 ]; then ## Checking the aspect ratio for cover image
@@ -18,6 +20,7 @@ while read -r manga; do
 	done < <(fd "jpg|png|jpeg" "$PWD/$manga" -t f)
 echo -e "[Desktop Entry]
 Name="$series $name"
+Comment=$chapter
 Exec=imv \"$PWD/$manga/\"* -s crop && swaymsg -t command workspace current
 Icon=$icon
 Type=Application" > "$HOME/Mangas/.Desktops/$manga.desktop"
