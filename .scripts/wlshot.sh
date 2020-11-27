290208e8b41ae0479a50cb16169022f220ca596c
Divyang <divyang_2k10@yahoo.com>
2020-11-27 12:43:36 +0530

    added appname to screenshot format

--- .scripts/wlshot.sh
+++ .scripts/wlshot.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
 ## Screenshots
+app_id="$(swaymsg -t get_tree | jq -r '.. | select (.pid? and .focused?) | .app_id')"
 
 case "$1" in
 	"-c")
 		grim -g "$(slurp -p -c "#FFFFFF" )" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:- | awk 'FNR==2 {print $3}' | wl-copy 
-		convert -size 120x50 xc:$(wl-paste) /tmp/color.png
-		notify-send -i /tmp/color.png $(wl-paste) --app-name="screenshots"
+		convert -size 96x96 xc:$(wl-paste) /tmp/color.png
+		notify-send "$(wl-paste)" -i /tmp/color.png --app-name=colorpicker
 		rm /tmp/color.png
 	;;
 	"-p")
-		grim -g "$(slurp -c "#FFFFFF" )" "$HOME/Pictures/shots/""$(date +"%F_%T").png" && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured" --app-name="screenshots" ## Partial screenshot 
+		grim -g "$(slurp -c "#FFFFFF" )" "$HOME/Pictures/shots/""$(date +"%F_%T")_"$app_id".png" && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured" --app-name="screenshots" ## Partial screenshot 
 		wl-copy < $(ls $HOME/Pictures/shots/* -drt | tail -n1)
 	;;
 	"-w")
-		grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -c "#FFFFFF" )" "$HOME/Pictures/shots/""$(date +"%F_%T").png" && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured" --app-name="screenshots"
+		grim -g "$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp -c "#FFFFFF" )" "$HOME/Pictures/shots/""$(date +"%F_%T")_"$app_id".png" && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured" --app-name="screenshots"
 		wl-copy < $(ls $HOME/Pictures/shots/* -drt | tail -n1)
 	;;
 	*)
-		grim "$HOME/Pictures/shots/""$(date +"%F_%T").png" -c && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured"  --app-name="screenshots" ## Normal screenshot  
+		grim "$HOME/Pictures/shots/""$(date +"%F_%T")_"$app_id".png" -c && notify-send -i $(ls $HOME/Pictures/shots/* -drt | tail -n1) "Screenshot Captured"  --app-name="screenshots" ## Normal screenshot  
 		wl-copy < $(ls $HOME/Pictures/shots/* -drt | tail -n1)
 	;;
 esac
