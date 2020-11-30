ee80ab7b243b499576e0f751232d69994dc953a7
Divyang <divyang_2k10@yahoo.com>
2020-11-30 20:07:28 +0530

    little changes to the preview script

--- .scripts/sniper.sh
+++ .scripts/sniper.sh
@@ -6,7 +6,7 @@ filetype="$(file -- "$pfile" | rg "directory|text" -o)"
 
 case "$filetype" in 
 		"text")
-			bat --theme=Nord -nf --line-range ":$FZF_PREVIEW_LINES" "$pfile"
+			bat --theme=Nord -nf --line-range ":$FZF_PREVIEW_LINES" "$pfile" -m "vimrc:VimL"
 			;;
 		"directory")
 			tree -FCL "$FZF_PREVIEW_COLUMNS" "$pfile"
